5769b86b04f8149041d79476028fe1b3b2e3183d
yunc98 <mr.yt510@gmail.com>
2023-01-24 15:55:33 +0900

    SearchBoxコンポーネント作成

---
+++ frontend/src/components/elements/SearchBox.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
+import Button from "./Button";
+
+const SearchBox = () => {
+  const [keyword, setKeyword] = useState<string>("");
+
+  const getResultData = () => {
+    console.log(keyword);
+  };
+
+  return (
+    <form className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 p-10">
+      <div className="mx-auto max-w-3xl flex justify-center">
+        <label htmlFor="search" className="sr-only">
+          検索
+        </label>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          className="block w-1/2 rounded-md border-gray-300 shadow-sm focus:border-yellow-500 focus:ring-yellow-500 sm:text-sm mr-2"
+          placeholder="発電機　藤沢市"
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+        <Button type="submit" onClick={getResultData}>検索</Button>
+      </div>
+    </form>
+  );
+};
+
+export default SearchBox;
