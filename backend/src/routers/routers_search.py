155df6ba517df1a5faaf30869a3b24b05a8b5ba9
Haruko-Fujita <haru.com.hf@gmail.com>
2023-01-25 20:14:23 +0900

    API_No.9 _serch.pyから未使用のinportを削除

--- backend/src/routers/routers_search.py
+++ backend/src/routers/routers_search.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, HTTPException
 from src.utils.logger.logger import setup_logger
-from fastapi.encoders import jsonable_encoder
 
 import src.cruds.cruds_search as search_crud
 
@@ -18,7 +17,9 @@ router = APIRouter(
 @router.get("/")
 def get_search_word(word: str):
     search_word_result = search_crud.get_search_word(key=word)
-    if not search_word_result: # listが空[]の場合
-        raise HTTPException(status_code=404, detail="一致する結果が見つかりませんでした。キーワードを変えて再検索してください。")
+    if not search_word_result:  # listが空[]の場合
+        raise HTTPException(
+            status_code=404, detail="一致する結果が見つかりませんでした。キーワードを変えて再検索してください。"
+        )
     logger.debug("検索router")
     return search_word_result
