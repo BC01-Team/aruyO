079d832bb4710fdc524511d2553544301f5eadbe
Haruko-Fujita <haru.com.hf@gmail.com>
2023-01-25 21:10:57 +0900

    API_No.7 物品詳細取得を作成

--- backend/src/routers/routers_exhibits.py
+++ backend/src/routers/routers_exhibits.py
@@ -18,5 +18,16 @@ def get_exhibits():
     exhibits_list = exhibits_crud.get_exhibits()
     if not exhibits_list:  # listが空[]の場合
         raise HTTPException(status_code=404, detail="出品物がありませんでした。")
-    logger.debug("出品物router")
+    logger.debug("出品物一覧router")
     return exhibits_list
+
+
+# API_No.6 出品物詳細取得
+@router.get("/{id}")
+def get_exhibit(id: str):
+    exhibit = exhibits_crud.get_exhibit(id=id)
+    logger.debug(exhibit)
+    if exhibit is None:
+        raise HTTPException(status_code=404, detail="指定の出品物がありませんでした。")
+    logger.debug("出品物詳細router")
+    return exhibit
