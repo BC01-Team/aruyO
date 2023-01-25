59fdebe86cfcb5c8c158040ad3d87ffa385d4fdc
yunc98 <mr.yt510@gmail.com>
2023-01-25 11:44:55 +0900

    Footerをページに適用

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '@/components/layouts/Header';
+import Footer from '@/components/layouts/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 };
