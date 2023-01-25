a2e27cc897d109549985ca41cfd4063323a55de4
tkskrt23 <tkskrt23@gmail.com>
2023-01-25 15:36:45 +0900

    ”/users/:id”パスのルーター設定の変更

--- backend/src/routers/routers_userInfo.py
+++ backend/src/routers/routers_userInfo.py
@@ -6,11 +6,14 @@ from fastapi.encoders import jsonable_encoder
 import src.cruds.cruds_usersInfo as usersInfo_crud
 
 logger = setup_logger(__name__)
-router = APIRouter()
+router = APIRouter(
+    prefix="/users",  # エンドポイントの頭のURL http://localhost:8080/exhibit
+    tags=["users"],  # http://127.0.0.1/docsの分類
+)
 
 
 # API_No.8-1 予約一覧取得(借りる予約)
-@router.get("/users/{id}/borrow")
+@router.get("/{id}/borrow")
 def get_borrow_list(id: str) -> list:
     logger.debug("borrowパス通過")
     res = usersInfo_crud.get_borrow_list(id=id)
@@ -20,7 +23,7 @@ def get_borrow_list(id: str) -> list:
 
 
 # API_No.8-2 予約一覧取得(貸す予約)
-@router.get("/users/{id}/lent")
+@router.get("/{id}/lent")
 def get_lent_list(id: str) -> list:
     logger.debug("lentパス通過")
     res = usersInfo_crud.get_lent_list(id=id)
