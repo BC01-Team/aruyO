97b13d5a1d7465c266f51c91532009034abeee79
yunc98 <mr.yt510@gmail.com>
2023-01-26 07:28:30 +0900

    検索結果の取得

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,33 @@
+import { useState, ChangeEvent } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
+import { axiosInstance } from '@/lib/axiosInstance';
 import SearchBox from '@/components/elements/SearchBox';
 
 export default function Home() {
+  const [keyword, setKeyword] = useState<string>("");
+  const [results, setResults] = useState();
+
+  // 検索結果が0件だった時の処理方法
+
+  const getResultData = async (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    console.log(keyword);
+
+    await axiosInstance
+      .get(`/search/?word=${keyword}`)
+      .then((res) => {
+        console.log(res.data);
+        setResults(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  console.log("results", results);
+
   return (
     <>
       <Head>
@@ -16,7 +41,20 @@ export default function Home() {
 
       <main>
         {/* デザインが来たら、随時修正 */}
-        <SearchBox />
+        <SearchBox setKeyword={setKeyword} getResultData={getResultData} />
+        {results && results.map((item) => {
+          return (
+            <Link
+              as={`/search/items/${item._id}`}
+              href={{ pathname: `/search/items/[id]`, query: item._id }}
+            >
+              <div key={item._id}>
+                <p>{item._id}</p>
+                <p>{item.info.name}</p>
+              </div>
+            </Link>
+          );
+        })}
       </main>
     </>
   );
