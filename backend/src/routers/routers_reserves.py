efc0a6ebd1d7e312ec493a7867d6bad06349ae78
tkskrt23 <tkskrt23@gmail.com>
2023-01-25 01:51:59 +0000

    コメント修正

--- backend/src/routers/routers_reserves.py
+++ backend/src/routers/routers_reserves.py
@@ -9,7 +9,7 @@ logger = setup_logger(__name__)
 router = APIRouter()
 
 
-# 予約登録（借りる側）
+# API_No.10 予約登録
 @router.post("/reserves")
 def create_reserve(request: Request, response: Response, data: dict):
     reserve = jsonable_encoder(data)
@@ -19,7 +19,7 @@ def create_reserve(request: Request, response: Response, data: dict):
     raise HTTPException(status_code=404, detail="予約登録ができませんでした")
 
 
-# 予約詳細取得
+# API_No.11 予約詳細取得
 @router.get("/reserves/{id}")
 def get_reserve(id: str):
     reserve = reserve_crud.get_reserve(id=id)
@@ -29,7 +29,7 @@ def get_reserve(id: str):
     return reserve
 
 
-# 予約情報変更
+# API_No.12 予約情報変更
 @router.put("/reserves/{id}")
 def update_reserve(id: str, data: dict):  # dataはrequestbodyにreserveコレクションから_idを抜いたものをいれた。余計な部分が多いのでステータスだけにしたい。
     reserve = jsonable_encoder(data)
