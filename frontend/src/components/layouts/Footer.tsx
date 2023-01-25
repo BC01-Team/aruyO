bfe44fc99b389918553a45f721437e47ed87e797
yunc98 <mr.yt510@gmail.com>
2023-01-25 11:43:12 +0900

    Footer作成

---
+++ frontend/src/components/layouts/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="bottom-0 bg-amber-50 absolute">
+      <div className="flex">
+        <div className="flex-1 w-screen">
+          <div className="flex justify-center ...">
+            <p className="text-center text-xs leading-5 text-gray-500 py-8">
+              &copy; 2023 aruyO, Inc. All rights reserved.
+           </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
