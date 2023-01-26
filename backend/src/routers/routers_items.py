277e37d3718d1f4020b9ff96dcb65f903c84de06
Haruko-Fujita <haru.com.hf@gmail.com>
2023-01-26 14:43:55 +0900

    API No.5/6/7 item 関連を_userInfo.pyに集約

--- backend/src/routers/routers_items.py
+++ backend/src/routers/routers_items.py
@@ -14,34 +14,5 @@ router = APIRouter(
 )
 
 
-# API_No.5 物品登録（company_id未取得）
-@router.post("/{id}/items")
-def create_item(request: Request, response: Response, data: dict):
-    item = jsonable_encoder(data)
-    res = items_crud.create_item(item)
-    logger.debug("物品登録router")
-    if res:
-        return res
-    raise HTTPException(status_code=404, detail="物品登録ができませんでした。")
 
 
-# API_No.6 物品一覧取得
-@router.get("/{id}/items")
-def get_user_items(id: str):
-    items_list = items_crud.get_user_items(id=id)
-    logger.debug(id)
-    logger.debug(items_list)
-    if not items_list:  # listが空[]の場合
-        raise HTTPException(status_code=404, detail="物品がありませんでした。")
-    logger.debug("ユーザ物品一覧router")
-    return items_list
-
-
-# API_No.7 物品詳細取得
-@router.get("/{id}/items/{item_id}")
-def get_user_item(item_id: str):
-    item = items_crud.get_user_item(item_id=item_id)
-    if item is None:
-        raise HTTPException(status_code=404, detail="指定の物品がありませんでした。")
-    logger.debug("ユーザー物品詳細router")
-    return item
--- backend/src/routers/routers_userInfo.py
+++ backend/src/routers/routers_userInfo.py
@@ -7,9 +7,42 @@ import src.cruds.cruds_usersInfo as usersInfo_crud
 
 logger = setup_logger(__name__)
 router = APIRouter(
-    prefix="/users",  # エンドポイントの頭のURL http://localhost:8080/exhibit
-    tags=["users"],  # http://127.0.0.1/docsの分類
+    prefix="/users",  # エンドポイントの頭のURL
+    tags=["users"],  # FastAPI Swagger /docsの分類
 )
+
+
+# API_No.5 物品登録（company_id未取得）
+@router.post("/{id}/items")
+def create_item(request: Request, response: Response, data: dict):
+    item = jsonable_encoder(data)
+    res = items_crud.create_item(item)
+    logger.debug("物品登録router")
+    if res:
+        return res
+    raise HTTPException(status_code=404, detail="物品登録ができませんでした。")
+
+
+# API_No.6 物品一覧取得
+@router.get("/{id}/items")
+def get_user_items(id: str):
+    items_list = usersInfo_crud.get_user_items(id=id)
+    logger.debug(id)
+    logger.debug(items_list)
+    if not items_list:  # listが空[]の場合
+        raise HTTPException(status_code=404, detail="物品がありませんでした。")
+    logger.debug("ユーザ物品一覧router")
+    return items_list
+
+
+# API_No.7 物品詳細取得
+@router.get("/{id}/items/{item_id}")
+def get_user_item(item_id: str):
+    item = usersInfo_crud.get_user_item(item_id=item_id)
+    if item is None:
+        raise HTTPException(status_code=404, detail="指定の物品がありませんでした。")
+    logger.debug("ユーザー物品詳細router")
+    return item
 
 
 # API_No.8-1 予約一覧取得(借りる予約)
@@ -19,7 +52,6 @@ def get_borrow_list(id: str) -> list:
     res = usersInfo_crud.get_borrow_list(id=id)
     logger.debug(res)
     return res
-
 
 
 # API_No.8-2 予約一覧取得(貸す予約)
