5b6484e475747fef332582d067ce4da5874f6f8e
tkskrt23 <tkskrt23@gmail.com>
2023-01-26 16:20:20 +0900

    予約一覧取得APIに認証セット

--- backend/src/routers/routers_userInfo.py
+++ backend/src/routers/routers_userInfo.py
@@ -1,30 +1,37 @@
-from fastapi import APIRouter, HTTPException
-from fastapi import Request, Response
+from fastapi import APIRouter, HTTPException, Cookie
 from src.utils.logger.logger import setup_logger
-from fastapi.encoders import jsonable_encoder
+from typing import Optional
 
 import src.cruds.cruds_usersInfo as usersInfo_crud
+import src.utils.auth.auth as auth
 
 logger = setup_logger(__name__)
 router = APIRouter(
-    prefix="/users",  # エンドポイントの頭のURL http://localhost:8080/exhibit
-    tags=["users"],  # http://127.0.0.1/docsの分類
+    prefix="/users",  # エンドポイントの頭のURL
+    tags=["users"],  # FastAPI Swagger /docsの分類
 )
 
 
 # API_No.8-1 予約一覧取得(借りる予約)
 @router.get("/{id}/borrow")
-def get_borrow_list(id: str) -> list:
-    logger.debug("borrowパス通過")
-    res = usersInfo_crud.get_borrow_list(id=id)
-    logger.debug(res)
-    return res
+def get_borrow_list(id: str, session_id: Optional[str] = Cookie(None)) -> list:
+    logger.debug("lentパス通過、認証前")
+    if auth.is_login(session_id):
+        logger.debug("lent認証後")
+        res = usersInfo_crud.get_borrow_list(id=id)
+        logger.debug(res)
+        return res
+    raise HTTPException(status_code=400, detail="ログイン情報がありません")
 
 
 
 # API_No.8-2 予約一覧取得(貸す予約)
 @router.get("/{id}/lent")
-def get_lent_list(id: str) -> list:
-    logger.debug("lentパス通過")
-    res = usersInfo_crud.get_lent_list(id=id)
-    return res
+def get_lent_list(id: str, session_id: Optional[str] = Cookie(None)) -> list:
+    logger.debug("lentパス通過、認証前")
+    if auth.is_login(session_id):
+        logger.debug("lent認証後")    
+        res = usersInfo_crud.get_lent_list(id=id)
+        logger.debug(res)
+        return res
+    raise HTTPException(status_code=400, detail="ログイン情報がありません")
