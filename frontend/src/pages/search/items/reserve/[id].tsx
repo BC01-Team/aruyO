7c2e0658ce84c08c904b5fa99bbca8064bb5e44d
yunc98 <mr.yt510@gmail.com>
2023-01-26 18:05:16 +0900

    予約ページで物品詳細の取得

--- frontend/src/pages/search/items/reserve/[id].tsx
+++ frontend/src/pages/search/items/reserve/[id].tsx
@@ -1,6 +1,33 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { axiosInstance } from "@/lib/axiosInstance";
+import { Item } from "@/types/item";
+import Button from "@/components/elements/Button";
+
 const ReserveItem = () => {
+  const router = useRouter();
+  const [item, setItem] = useState<Item>();
+
+  useEffect(() => {
+    const itemId = router.query.id;
+
+    axiosInstance
+      .get(`/items/${itemId}`)
+      .then((res) => {
+        setItem(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <h1>予約画面</h1>
+    <div>
+      <h1>予約画面</h1>
+      <p>{item?.info?.name}</p>
+      <p>{item?.info?.price}</p>
+    </div>
   );
 };
 
