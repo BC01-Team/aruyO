8342db3c100f942685131ff2ef945d07669c2da7
Haruko-Fujita <haru.com.hf@gmail.com>
2023-01-26 11:17:39 +0900

    exhibitをitemに変更(API/seed/main)

--- backend/src/cruds/cruds_items.py
+++ backend/src/cruds/cruds_items.py
@@ -8,35 +8,35 @@ from src.utils.serializer.serializer import db_collection_serializer
 logger = setup_logger(__name__)
 
 # 出品物コレクションを取得
-collection_exhibits = db.exhibits
+collection_items = db.items
 
 
 # API_No.5 出品物登録（company_id未取得）
-def create_exhibit(data: dict) -> Union[dict, bool]:
+def create_item(data: dict) -> Union[dict, bool]:
     logger.debug("出品物登録crud")
     # mongoDB insert_oneでドキュメント登録後、find_oneで登録値を取得
-    exhibit = collection_exhibits.insert_one(data)
-    new_exhibit = collection_exhibits.find_one({"_id": exhibit.inserted_id})
-    if new_exhibit:
-        return db_collection_serializer(new_exhibit)
+    item = collection_items.insert_one(data)
+    new_item = collection_items.find_one({"_id": item.inserted_id})
+    if new_item:
+        return db_collection_serializer(new_item)
     return False
 
 
 # API_No.6 出品物一覧取得
-def get_exhibits():
+def get_items():
     logger.debug("出品物一覧crud")
     # mongoDB findでドキュメント取得し、listに追加
-    exhibits = collection_exhibits.find()
-    exhibits_list = []
-    for document in exhibits:
+    items = collection_items.find()
+    items_list = []
+    for document in items:
         logger.debug(document)
-        exhibits_list.append(db_collection_serializer(document))
-    return exhibits_list
+        items_list.append(db_collection_serializer(document))
+    return items_list
 
 
 # API_No.7 出品物詳細取得
-def get_exhibit(id: str):
+def get_item(id: str):
     logger.debug("出品物詳細crud")
     # mongoDB findでドキュメント取得
-    exhibit = collection_exhibits.find_one({"_id": ObjectId(id)})
-    return db_collection_serializer(exhibit)
+    item = collection_items.find_one({"_id": ObjectId(id)})
+    return db_collection_serializer(item)
