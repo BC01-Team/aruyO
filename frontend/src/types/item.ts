b649c2ee3ec11a6e1598bd08ccd6a9a5f0812dd5
yunc98 <mr.yt510@gmail.com>
2023-01-26 12:15:20 +0900

    item型定義ファイルの追加

---
+++ frontend/src/types/item.ts
@@ -0,0 +1,14 @@
+export type Item = {
+  _id?: string,
+  info?: {
+    name: string,
+    picture: string,
+    detail: string,
+    requirements: string,
+    take_out: string,
+    price: string,
+    address: string
+  },
+  location?: Array<number>, // 型 要確認
+  company_id?: string
+};
