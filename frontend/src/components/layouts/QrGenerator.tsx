58c77141eed3a83ee5ae553d4b8762289e0e400e
OakIMino <m.ooki20220615@gmail.com>
2023-01-27 13:24:41 +0900

    front:QRcode生成と読み取り

---
+++ frontend/src/components/layouts/QrGenerator.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useAuth } from "../../context/auth";
+import QrImage from "../elements/QrImage"
+
+type Props = { qrText: string };
+
+const QrGenerator = ({ qrText }: Props) => {
+  return (
+    <>
+      <QrImage qrText={qrText} />
+    </>
+  );
+};
+
+export default QrGenerator;
