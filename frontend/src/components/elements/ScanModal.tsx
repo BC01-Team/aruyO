767119b6e64b9847384c81414ac2471ffc05cd37
OakIMino <m.ooki20220615@gmail.com>
2023-01-27 16:27:04 +0900

    logoutのapi実装

--- frontend/src/components/elements/ScanModal.tsx
+++ frontend/src/components/elements/ScanModal.tsx
@@ -1,7 +1,13 @@
 import QrReader  from "./QrCamera";
 
+type Props = {
+  open: boolean;
+  onRequestClose: () => void;
+  setResult: React.Dispatch<React.SetStateAction<string>>
+};
 
-export default function ScanModal({ open, onRequestClose, setResult }) {
+
+export default function ScanModal({ open, onRequestClose, setResult }:Props) {
   return (
     <>
       {open ? (
