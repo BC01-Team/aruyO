c27d493cadb52eddcd04c3e21b10eaa8cf01d35e
tkskrt23 <tkskrt23@gmail.com>
2023-01-29 01:07:28 +0900

    地理空間インデックス作成のコード修正（コレクション名）

--- backend/src/create_geospatial.py
+++ backend/src/create_geospatial.py
@@ -2,11 +2,11 @@ from pymongo import GEO2D
 from db import db
 
 collection_companies = db.companies
-collection_exhibits = db.exhibits
+collection_items = db.items
 
 # コンテナ初回作成時にfastapiコンテナ内のapp/srcで```# python create_geospatial.py```を実行 
 
 # companyコレクションとexhibitsコレクションのlcationキーにgeospatialインデックスを作成する。
 collection_companies.create_index([("info.location", GEO2D)])
-collection_exhibits.create_index([("location", GEO2D)])
+collection_items.create_index([("location", GEO2D)])
 
