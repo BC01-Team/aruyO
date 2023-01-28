8cc1967e06eeb2777621024b3359010a0d5d7803
OakIMino <m.ooki20220615@gmail.com>
2023-01-28 15:52:50 +0900

    'Duplicate atom key'error修正

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withInterceptStdout = require("next-intercept-stdout");
+const nextConfig = withInterceptStdout({
   reactStrictMode: true,
-}
-
+},
+  (text) => (text.includes('Duplicate atom key') ? '' : text),
+);
 module.exports = nextConfig
