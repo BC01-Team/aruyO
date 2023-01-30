3ffebf4e3eb01ed40ccc6e065558d51d6469b725
Haruko-Fujita <haru.com.hf@gmail.com>
2023-01-30 13:43:07 +0900

    front myPage items詳細にAPI呼び出しを追加

--- frontend/src/pages/mypage/items/[id].tsx
+++ frontend/src/pages/mypage/items/[id].tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import Sidebar from "@/components/layouts/mypage/Sidebar";
 import MypageLayout from "@/components/layouts/mypage/MypageLayout";
 import PageTitle from "@/components/layouts/mypage/PageTitle";
@@ -32,7 +33,7 @@ const itemId = "63d607e9f7e435916eb7ae51";
 const MypageItemDetail = ({ result }: OrderProps) => {
   const router = useRouter();
   const [hydrated, setHydrated] = useState(false);
-  const [items, setItems] = useState(null);
+  const [item, setItem] = useState(null);
   const [loading, setLoading] = useState(false);
   const user = useRecoilValue(userState);
 
@@ -49,7 +50,7 @@ const MypageItemDetail = ({ result }: OrderProps) => {
           })
         ).data;
 
-        setItems(res);
+        setItem(res);
         setLoading(false);
       };
 
@@ -62,13 +63,59 @@ const MypageItemDetail = ({ result }: OrderProps) => {
   return (
     <ProtectRoute>
       <>
-        {!loading && items && (
+        {!loading && item && (
           <>
             <Sidebar />
             <MypageLayout>
-              <PageTitle>{router.query.id}</PageTitle>
+              <PageTitle>ユーザー物品詳細{router.query.id}</PageTitle>
               <ContentsLayout>
-                <p>詳細ページ</p>
+                <div className="overflow-hidden bg-white shadow sm:rounded-md">
+                  <ul role="list" className="divide-y divide-gray-200">
+                    <li key="index">
+                      <Link
+                        as={`/mypage/items/${item._id}`}
+                        href={{
+                          pathname: `/mypage/items/[id]`,
+                          query: item._id,
+                        }}
+                      >
+                        <div className="flex items-center px-4 py-4 sm:px-6">
+                          <div className="flex min-w-0 flex-1 items-center">
+                            <div className="flex-shrink-0">
+                              <img
+                                className="h-12 w-12 rounded-full"
+                                src={item.info.pictures[0]}
+                                alt=""
+                              />
+                            </div>
+                            <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
+                              <div>
+                                <p className="truncate text-sm font-medium text-amber-600">
+                                  {item.info.name}
+                                </p>
+                                <p className="mt-2 flex items-center text-sm text-gray-500">
+                                  <span className="truncate">
+                                    {item.info.price}円
+                                  </span>
+                                </p>
+                              </div>
+                              <div className="hidden md:block">
+                                <div>
+                                  <p className="text-sm text-gray-900">
+                                    表示させる項目は要検討
+                                  </p>
+                                  <p className="mt-2 flex items-center text-sm text-gray-500">
+                                    {item.info.address}
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
               </ContentsLayout>
             </MypageLayout>
           </>
