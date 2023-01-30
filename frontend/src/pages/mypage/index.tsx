ed8328f1ea8d5ce327cb1a6cee5ed05a4f7a9a86
Haruko-Fujita <haru.com.hf@gmail.com>
2023-01-30 13:28:55 +0900

    front myPage itemsのAPI呼び出しでuserIdを認証情報から取得に変更

--- frontend/src/pages/mypage/index.tsx
+++ frontend/src/pages/mypage/index.tsx
@@ -1,28 +1,12 @@
-import axios from "axios";
-import { useState, ChangeEvent } from "react";
 import { useState, useEffect } from "react";
 import Sidebar from "@/components/layouts/mypage/Sidebar";
 import MypageLayout from "@/components/layouts/mypage/MypageLayout";
 import PageTitle from "@/components/layouts/mypage/PageTitle";
 import ContentsLayout from "@/components/layouts/mypage/ContentsLayout";
 import ProtectRoute from "@/components/layouts/ProtectRoute";
-
-// 認証周りができたら、動的にする。テスト用のため矛盾あり。
-const user_id = "63d607e9f7e435916eb7ae4e";
-
-// export async function getServerSideProps() {
-//   const ENDPOINT = "http://localhost:8888/users/63d607e9f7e435916eb7ae4e";
-
-//   const data = await axios.get(ENDPOINT).then((res) => res.data);
-
-//   // 404や500を受け取ればレスポンスがあるので正常と判断しエラーは出ず .thenに進む。
-//   // const data = await fetch(ENDPOINT).then(res => res.json());
-//   console.log(data);
-
-//   return {
-//     props: { data },
-//   };
-// }
+import { useRecoilValue } from "recoil";
+import { userState } from "@/lib/atom";
+import { axiosInstance } from "@/lib/axiosInstance";
 
 const user = [
   {
@@ -54,9 +38,27 @@ const user = [
 
 const Mypage = () => {
   const [hydrated, setHydrated] = useState(false);
+  const [userInfo, setUserInfo] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const user = useRecoilValue(userState);
 
   useEffect(() => {
     setHydrated(true);
+    setLoading(true);
+    if (user) {
+      const userId = user.id;
+      const fetchDate = async () => {
+        const res = await (
+          await axiosInstance.get(`/users/${userId}`, {
+            withCredentials: true,
+          })
+        ).data;
+
+        setUserInfo(res);
+        setLoading(false);
+      };
+      fetchDate();
+    }
   }, []);
 
   if (!hydrated) return null;
@@ -64,91 +66,88 @@ const Mypage = () => {
   return (
     <ProtectRoute>
       <>
-        <Sidebar />
-        <MypageLayout>
-          <PageTitle>マイページトップ</PageTitle>
-          <ContentsLayout>
-            <div className="overflow-hidden bg-white shadow sm:rounded-md">
-            <ul role="list" className="divide-y divide-gray-200">
-              {user.map((user, index) => {
-                return (
-                  <li key={index}>
-                    <div className="flex user-center px-4 py-4 sm:px-6">
-                      <div className="flex min-w-0 flex-1 user-center">
-                        <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
-                          <div>
-                            <p className="truncate text-sm font-medium text-amber-600">
-                              {user.info.name}
-                            </p>
-                            <p className="mt-2 flex user-center text-sm text-gray-500">
-                              <span className="truncate">
-                                {user.info.address}
-                              </span>
-                            </p>
-                          </div>
-                          <div className="hidden md:block">
+        {!loading && userInfo && (
+          <>
+            <Sidebar />
+            <MypageLayout>
+              <PageTitle>マイページトップ</PageTitle>
+              <ContentsLayout>
+                <div className="overflow-hidden bg-white shadow sm:rounded-md">
+                  <ul role="list" className="divide-y divide-gray-200">
+                    <li key="index">
+                      <div className="flex user-center px-4 py-4 sm:px-6">
+                        <div className="flex min-w-0 flex-1 user-center">
+                          <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
                             <div>
-                              <p className="text-sm text-gray-900">
-                                表示させる項目は要検討
-                              </p>
-                              <a
-                                className="mt-2 flex user-center text-sm text-gray-500"
-                                href={user.info.hp_url}
-                              >
-                                {user.info.hp_url}
-                              </a>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        </ContentsLayout>
-        <ContentsLayout>
-          <div className="overflow-hidden bg-white shadow sm:rounded-md">
-            <ul role="list" className="divide-y divide-gray-200">
-              {user.map((user, index) => {
-                return (
-                  <li key={index}>
-                    <div className="flex user-center px-4 py-4 sm:px-6">
-                      <div className="flex min-w-0 flex-1 user-center">
-                        <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
-                          <div>
-                            <p className="truncate text-sm font-medium text-amber-600">
-                              {user.staff[0].name}
-                            </p>
-                            <p>
-                              管理者 {user.staff[0].role.admin} / 貸す{" "}
-                              {user.staff[0].role.item} / 借りる{" "}
-                              {user.staff[0].role.reservation}
-                            </p>
-                          </div>
-                          <div className="hidden md:block">
-                            <div>
-                              <p className="text-sm text-gray-900">
-                                表示させる項目は要検討
+                              <p className="truncate text-sm font-medium text-amber-600">
+                                {userInfo.info.name}
                               </p>
                               <p className="mt-2 flex user-center text-sm text-gray-500">
                                 <span className="truncate">
-                                  {user.staff[0].email}
+                                  {userInfo.info.address}
                                 </span>
                               </p>
+                            </div>
+                            <div className="hidden md:block">
+                              <div>
+                                <p className="text-sm text-gray-900">
+                                  表示させる項目は要検討
+                                </p>
+                                <a
+                                  className="mt-2 flex user-center text-sm text-gray-500"
+                                  href={userInfo.info.hp_url}
+                                >
+                                  {userInfo.info.hp_url}
+                                </a>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          </ContentsLayout>
-        </MypageLayout>
+                    </li>
+                  </ul>
+                </div>
+              </ContentsLayout>
+              <ContentsLayout>
+                <div className="overflow-hidden bg-white shadow sm:rounded-md">
+                  <ul role="list" className="divide-y divide-gray-200">
+                    <li key="index">
+                      <div className="flex user-center px-4 py-4 sm:px-6">
+                        <div className="flex min-w-0 flex-1 user-center">
+                          <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
+                            <div>
+                              <p className="truncate text-sm font-medium text-amber-600">
+                                {userInfo.staff[0].name}
+                              </p>
+                              <p>
+                                管理者 {userInfo.staff[0].role.admin} / 貸す{" "}
+                                {userInfo.staff[0].role.item} / 借りる{" "}
+                                {userInfo.staff[0].role.reservation}
+                              </p>
+                            </div>
+                            <div className="hidden md:block">
+                              <div>
+                                <p className="text-sm text-gray-900">
+                                  表示させる項目は要検討
+                                </p>
+                                <p className="mt-2 flex user-center text-sm text-gray-500">
+                                  <span className="truncate">
+                                    {userInfo.staff[0].email}
+                                  </span>
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </ContentsLayout>
+            </MypageLayout>
+          </>
+        )}
+        {loading && <div>ロード中</div>}
       </>
     </ProtectRoute>
   );
--- frontend/src/pages/mypage/items/[id].tsx
+++ frontend/src/pages/mypage/items/[id].tsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { axiosInstance } from "@/lib/axiosInstance";
 import Sidebar from "@/components/layouts/mypage/Sidebar";
 import MypageLayout from "@/components/layouts/mypage/MypageLayout";
 import PageTitle from "@/components/layouts/mypage/PageTitle";
 import ContentsLayout from "@/components/layouts/mypage/ContentsLayout";
 import ProtectRoute from "@/components/layouts/ProtectRoute";
-
+import { Order } from "@/types/order";
+//componentで使用する際下記記載
+import { useRecoilState, useRecoilValue } from "recoil";
+import { userState } from "@/lib/atom";
+import { axiosInstance } from "@/lib/axiosInstance";
 
 // type Item = {
 //   _id?: string;
@@ -24,38 +27,53 @@ import ProtectRoute from "@/components/layouts/ProtectRoute";
 //   company_id: string;
 // }
 
-const MypageItemDetail = () => {
+const itemId = "63d607e9f7e435916eb7ae51";
+
+const MypageItemDetail = ({ result }: OrderProps) => {
   const router = useRouter();
   const [hydrated, setHydrated] = useState(false);
-  // const [item, setItem] = useState<Item>();
+  const [items, setItems] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const user = useRecoilValue(userState);
 
   useEffect(() => {
-    const itemId = router.query.id;
-
-    axiosInstance
-      .get("/")
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    
     setHydrated(true);
-  }, []);
+    setLoading(true);
+    if (user) {
+      const userId = user.id;
+      const fetchDate = async () => {
+        // 物品詳細取得
+        const res = await (
+          await axiosInstance.get(`/users/${userId}/items/${itemId}`, {
+            withCredentials: true,
+          })
+        ).data;
+
+        setItems(res);
+        setLoading(false);
+      };
+
+      fetchDate();
+    }
+  }, [itemId]);
 
   if (!hydrated) return null;
 
   return (
     <ProtectRoute>
       <>
-        <Sidebar />
-        <MypageLayout>
-          <PageTitle>{router.query.id}</PageTitle>
-          <ContentsLayout>
-            <p>詳細ページ</p>
-          </ContentsLayout>
-        </MypageLayout>
+        {!loading && items && (
+          <>
+            <Sidebar />
+            <MypageLayout>
+              <PageTitle>{router.query.id}</PageTitle>
+              <ContentsLayout>
+                <p>詳細ページ</p>
+              </ContentsLayout>
+            </MypageLayout>
+          </>
+        )}
+        {loading && <div>ロード中</div>}
       </>
     </ProtectRoute>
   );
