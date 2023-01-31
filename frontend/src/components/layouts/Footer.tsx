5c70ebf142fd1872c5bb223921cbafdd51c9aaa0
tkskrt23 <tkskrt23@gmail.com>
2023-01-31 16:29:01 +0900

    footerのCSS設定

--- frontend/src/components/layouts/Footer.tsx
+++ frontend/src/components/layouts/Footer.tsx
@@ -1,10 +1,13 @@
 const Footer = () => {
   return (
-    <footer className="bottom-0 bg-amber-50 absolute">
+    <footer className="bottom-0 bg-[#EBEBEB] absolute">
       <div className="flex">
         <div className="flex-1 w-screen">
-          <div className="flex justify-center ...">
-            <p className="text-center text-xs leading-5 text-gray-500 py-8">
+          <div className="flex flex-col justify-center p-6">
+            <p className="text-center text-xs leading-5">
+              利用規約 | プライバシーポリシー | プライバシーステートメント | 健全化に資する運用方針 | お問い合わせ | 運営会社 | サイトマップ | フリーワードで探す | 特定商取引法の表示
+            </p>
+            <p className="text-center text-sm leading-5">
               &copy; 2023 aruyO, Inc. All rights reserved.
            </p>
           </div>
