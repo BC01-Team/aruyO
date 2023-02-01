1c82e0659572264d9b2f062ab33b527efe2ada02
OakIMino <m.ooki20220615@gmail.com>
2023-02-01 20:15:44 +0900

    ProtectRoute挿入

--- frontend/src/pages/mypage/orders/borrow/evaluation/[id].tsx
+++ frontend/src/pages/mypage/orders/borrow/evaluation/[id].tsx
@@ -7,6 +7,7 @@ import React, {
 } from "react";
 import EvaluationButton from "@/components/layouts/mypage/orders/Evaluation";
 import PageTitle from "@/components/layouts/mypage/PageTitle";
+import ProtectRoute from "@/components/layouts/ProtectRoute";
 
 const Evaluation = () => {
   const [hydrated, setHydrated] = useState(false);
@@ -16,19 +17,21 @@ const Evaluation = () => {
   if (!hydrated) return null;
 
   return (
-    <>
-      <div className="bg-white">
-        <div className="flex flex-col items-center">
-          <PageTitle>取引評価</PageTitle>
-          <div className="text-lg mt-5 font-semibold">
-            <h1>貸した人を評価してください</h1>
-          </div>
-          <div className="mt-3">
-            <EvaluationButton type={"lender"} />
+    <ProtectRoute>
+      <>
+        <div className="bg-white">
+          <div className="flex flex-col items-center">
+            <PageTitle>取引評価</PageTitle>
+            <div className="text-lg mt-5 font-semibold">
+              <h1>貸した人を評価してください</h1>
+            </div>
+            <div className="mt-3">
+              <EvaluationButton type={"lender"} />
+            </div>
           </div>
         </div>
-      </div>
-    </>
+      </>
+    </ProtectRoute>
   );
 };
 
