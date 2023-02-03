fec6ea6ef29dbda5eca2337606be2c78f1227c4e
tkskrt23 <tkskrt23@gmail.com>
2023-02-03 09:51:12 +0900

    未ログイン時に使用するため認証を削除

--- backend/src/routers/items.py
+++ backend/src/routers/items.py
@@ -12,31 +12,20 @@ router = APIRouter(
 )
 
 
-# API_No.6-2 全ユーザーの物品一覧取得
+# API_No.6-2 全ユーザーの物品一覧取得　未ログイン時も使用するため認証はなし
 @router.get("/")
-def get_all_items(session_id: Optional[str] = Cookie(None)) -> list:
-    logger.debug("get_all_itemsパス通過、認証前")
-    if auth.is_login(session_id):
-        logger.debug("get_all_items認証後")
-        res = items_crud.get_all_items()
-        if not res:  # listが空[]の場合
-            raise HTTPException(status_code=404, detail="物品がありませんでした。")
-        logger.debug(res)
-        return res
-    raise HTTPException(status_code=400, detail="ログイン情報がありません")
+def get_all_items() -> list:
+    res = items_crud.get_all_items()
+    if not res:  # listが空[]の場合
+        raise HTTPException(status_code=404, detail="物品がありませんでした。")
+    logger.debug(res)
+    return res
 
-
-# API_No.7-2 物品詳細取得
+# API_No.7-2 物品詳細取得　未ログイン時も使用するため認証はなし
 @router.get("/{id}")
-def get_item(
-    id: str, session_id: Optional[str] = Cookie(None)
-) -> dict:
-    logger.debug("get_itemパス通過、認証前")
-    if auth.is_login(session_id):
-        logger.debug("get_item認証後")
-        res = items_crud.get_item(id=id)
-        if res is None:
-            raise HTTPException(status_code=404, detail="指定の物品がありませんでした。")
-        logger.debug(res)
-        return res
-    raise HTTPException(status_code=400, detail="ログイン情報がありません")
+def get_item(id: str) -> dict:
+    res = items_crud.get_item(id=id)
+    if res is None:
+        raise HTTPException(status_code=404, detail="指定の物品がありませんでした。")
+    logger.debug(res)
+    return res
